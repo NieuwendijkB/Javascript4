cab3387a0d2ba524ef83438e8107645fb3ba7525
NieuwendijkB <93547503+NieuwendijkB@users.noreply.github.com>
2021-12-07 20:28:15 +0100

    Add files via upload

---
+++ If_else.js
@@ -0,0 +1,33 @@
+const age = 19;
+if (age >= 18 && age <= 25) {
+  console.log("You get 50% off!");
+} else {
+  console.log("Full price for you!");
+}
+
+const isFemale = true;
+if (isFemale) {
+  console.log("drinks 50% off");
+} else {
+  console.log("full price for you buddy");
+}
+
+const driverStatus = "bob";
+if (driverStatus == "bob") {
+  console.log("Here are your keys legend");
+} else {
+  console.log("Start walking Drinky Mc Alcoholface.");
+}
+
+const firstName = "Sarah";
+if (firstName == "Sarah" || firstName == "Bram") {
+  console.log("Free beer!");
+}
+const totalAmount = 33;
+if (totalAmount >= 25 && totalAmount < 50) {
+  console.log("Free veggie burger!");
+} else if (totalAmount >= 50 && totalAmount < 100) {
+  console.log("Free Nachos!");
+} else if (totalAmount >= 100) {
+  console.log("Free bottle of champagne!");
+}
